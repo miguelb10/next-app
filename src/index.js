7b810eb138cc2f8dbe024da178df0cfe2e8b3bac
miguelb10 <miguel.blas_03@hotmail.com>
2021-08-09 01:07:22 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
